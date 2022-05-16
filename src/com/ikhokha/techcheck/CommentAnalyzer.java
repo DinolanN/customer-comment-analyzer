ec9f5e8f18e8e278fbac3db17d4c63b98ef57fb6
DinolanN <dino.naidoo27@gmail.com>
2022-05-16 15:37:36 +0200

    Finished the Tech Assessment

--- src/com/ikhokha/techcheck/CommentAnalyzer.java
+++ src/com/ikhokha/techcheck/CommentAnalyzer.java
@@ -5,12 +5,17 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class CommentAnalyzer {
 	
 	private File file;
+	private List<SearchMetrics> searchMetrics;
+	
+	private Map<String, Integer> resultsMap = new HashMap<>();
 	
 	public CommentAnalyzer(File file) {
 		this.file = file;
@@ -18,25 +23,57 @@ public class CommentAnalyzer {
 	
 	public Map<String, Integer> analyze() {
 		
-		Map<String, Integer> resultsMap = new HashMap<>();
+		searchMetrics = SetSearchParams();
+		
+		resultsMap = InitializeMap(resultsMap, searchMetrics);
 		
 		try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
 			
 			String line = null;
 			while ((line = reader.readLine()) != null) {
-				
-				if (line.length() < 15) {
+				//Loop for check the files according to the search metrics
+				for (SearchMetrics metric: searchMetrics) {
 					
-					incOccurrence(resultsMap, "SHORTER_THAN_15");
+					//Switch case depending on the seach metric types
+					switch(metric.Type.toLowerCase()) {
+					
+						case "int":
+							
+							if (metric.Name.toLowerCase().contains("shorter")) {
+								
+								if (line.length() < Integer.parseInt(metric.Value)) {
+									
+									incOccurrence(resultsMap, metric.Name);
 
-				} else if (line.contains("Mover")) {
+								}
+								
+							} else if (metric.Name.toLowerCase().contains("longer")) {
+								
+								if (line.length() >= Integer.parseInt(metric.Value)) {
+									
+									incOccurrence(resultsMap, metric.Name);
 
-					incOccurrence(resultsMap, "MOVER_MENTIONS");
-				
-				} else if (line.contains("Shaker")) {
-
-					incOccurrence(resultsMap, "SHAKER_MENTIONS");
-				
+								}
+								
+							}
+							
+							break;
+						
+						case "string":
+							
+							if (line.toLowerCase().contains(metric.Value.toLowerCase())) {
+								
+								incOccurrence(resultsMap, metric.Name);
+								
+							}
+							
+							break;
+							
+						default:
+							
+							break;
+							
+					}
 				}
 			}
 			
@@ -62,5 +99,35 @@ public class CommentAnalyzer {
 		countMap.putIfAbsent(key, 0);
 		countMap.put(key, countMap.get(key) + 1);
 	}
-
+	
+	//Initialize the map and set the metrics to 0
+	private Map<String, Integer> InitializeMap (Map<String, Integer> countMap, List<SearchMetrics> searchMetrics) {
+		
+		for (SearchMetrics metric : searchMetrics) {
+			countMap.putIfAbsent(metric.Name, 0);
+		}
+		
+		return countMap;
+		
+	}
+	
+	//Add custom objects(search parameters) to the list of search metrics
+	private List<SearchMetrics> SetSearchParams () {
+		
+		List<SearchMetrics> searchMetrics = new ArrayList<>();
+		
+		SearchMetrics searchMetric1 = new SearchMetrics("SHORTER_THAN_15", "int", "15");
+		SearchMetrics searchMetric2 = new SearchMetrics("MOVER_MENTIONS", "string", "mover");
+		SearchMetrics searchMetric3 = new SearchMetrics("SHAKER_MENTIONS", "string", "shaker");
+		SearchMetrics searchMetric4 = new SearchMetrics("QUESTIONS", "string", "?");
+		SearchMetrics searchMetric5 = new SearchMetrics("SPAM", "string", "http");
+		searchMetrics.add(searchMetric1);
+		searchMetrics.add(searchMetric2);
+		searchMetrics.add(searchMetric3);
+		searchMetrics.add(searchMetric4);
+		searchMetrics.add(searchMetric5);
+		
+		return searchMetrics;
+		
+	}
 }
---
+++ src/com/ikhokha/techcheck/SearchMetrics.java
@@ -0,0 +1,17 @@
+package com.ikhokha.techcheck;
+
+public class SearchMetrics {
+	
+	public String Name;
+	
+	public String Type;
+	
+	public String Value;
+	
+	public SearchMetrics (String name, String type, String value) {
+		this.Name = name;
+		this.Type = type;
+		this.Value = value;
+	}
+
+}
